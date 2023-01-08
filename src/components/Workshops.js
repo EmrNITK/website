f0380d0cb5c832607aef315666b3ad5ea0d89cbf
HarshKakran <harshkakran05@gmail.com>
2023-01-08 12:41:24 +0530

    Added basic structure, not adding routes as of now

---
+++ src/components/Workshops.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Workshops(){
+
+  return (
+    <section id="workshops">
+    
+    </section>
+  )
+}
---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const team = [
+  {
+    name: "Harsh",
+    pos: "Member",
+    img: "img_path",
+    linkedin_profile: "https://www.linkedin.com/in/harshkakran/",
+    github_profile: "github.com/HarshKakran/",
+    email: "harshkakran05@gmail.com"
+  }
+];
+
+export const events = [
+  {
+    img: "img_path",
+    name: "RoboSoccer",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    rule_book: "url",
+  }
+]
+
+export const workshop = [
+  {
+    img: "img_path",
+    topic: "dip"
+  }
+]
