a8a45a494437b1c9329955c78b108f16a285524c
mr-prometheus <deepuadak@gmail.com>
2023-01-12 01:27:43 +0530

    Added Team components working with Carousel

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,10 +1,78 @@
 import React from "react";
-
+import Carousel from 'react-multi-carousel';
+import './styles/team.css';
+import './styles/card.css';
+import 'react-multi-carousel/lib/styles.css';
+import Card from "./Teamcard";
 export default function Team(){
-
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
   return (
-    <section id="team">
-
+    <section className = "team" id="teams">
+      <div className="container_2">
+            <div className="row">
+                <div className="col-12">
+                    <div className="skill-bx wow zoomIn">
+                        <p className = "team-heading">Team</p>
+                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                          <Card
+                          name= "Lorem Ipsum"
+                          img= ""
+                          linkedin="xyz"
+                          github="https://github.com/EmrNITK/website" 
+                          insta = "xyz"
+                          />
+                          <Card
+                          name= "Lorem Ipsum"
+                          img= ""
+                          linkedin="xyz"
+                          github="xyz" 
+                          insta = "xyz"
+                          />
+                          <Card
+                          name= "Lorem Ipsum"
+                          img= ""
+                          linkedin="xyz"
+                          github="xyz" 
+                          insta = "xyz"
+                          />
+                          <Card
+                          name= "Lorem Ipsum"
+                          img= ""
+                          linkedin="xyz"
+                          github="xyz" 
+                          insta = "xyz"
+                          />
+                          <Card
+                          name= "Lorem Ipsum"
+                          img= ""
+                          linkedin="xyz"
+                          github="xyz" 
+                          insta = "xyz"
+                          />
+                        </Carousel>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     </section>
   )
 }
---
+++ src/components/Teamcard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './styles/card.css';
+import navIcon1 from '../assets/nav-icon1.svg';
+import navIcon3 from '../assets/nav-icon3.svg';
+import navIcon4 from '../assets/nav-icon4.svg';
+function Card(props) {
+  return (
+    <div className="card">
+      
+        <img className="circle-img" src={props.img} alt="avatar_img" />
+        <p className="name">{props.name}</p>
+      
+     
+        <div className="social-icon">
+                <div className = "icon-div"><a  href={props.linkedin}><img src={navIcon1} alt="linkedin" /></a></div>
+                <div className = "icon-div"><a href={props.github}><img src={navIcon4} alt="github" /></a></div>
+                <div className = "icon-div"><a href={props.insta}><img src={navIcon3} alt="instagram" /></a></div>
+              </div>
+    </div>
+  );
+}
+
+export default Card;
