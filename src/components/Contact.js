979c361c48bdb0b80183c5aec5c662bc490a6bd0
Ekant Yadav <ekantyadav001@gmail.com>
2023-02-08 00:31:11 +0530

    Fixed Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,7 +15,7 @@ function contact() {
                 <div className="icon"><i className="fas fa-map-marker-alt"></i></div>
                 <div className="text">
                     <h3>Address</h3>
-                    <p> Nit Kurukshetra<br/>Kurukshetra,Haryana<br/>136119</p>
+                    <p> Nit Kurukshetra<br/>Kurukshetra,Haryana</p>
                 </div>
             </div>
             <div className="box">
@@ -29,11 +29,11 @@ function contact() {
                 <div className="icon"><i className ="fas fa-envelope" ></i></div>
                 <div className="text">
                     <h3>Email</h3>
-                    <p>rohan_12015150@nitkkr.ac.in</p>
+                    <p>emr@nitkkr.ac.in</p>
                 </div>
             </div>
         </div>
-        <div className="contactForm">
+        {/* <div className="contactForm">
             <form>
                 <h2>Send Message</h2>
                 <div className="inputBox">
@@ -53,7 +53,8 @@ function contact() {
 
                 </div>
             </form>
-        </div>
+        </div> */}
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6914.336852476472!2d76.81218044835846!3d29.945833673846664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390e3f51bab18cb5%3A0x96200205143e35f6!2sNIT%2C%20Thanesar%2C%20Haryana%20136119!5e0!3m2!1sen!2sin!4v1675796177740!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="map"></iframe>
     </div>
   </section>
         </>
