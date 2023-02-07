7d2f73c6eacbac8fc60869cda59322c5c92e683f
yujit2003 <yujityadav99@gmail.com>
2023-02-07 19:02:40 +0530

    Few Changes in class name as it was clashing with other components css

--- src/components/Events.js
+++ src/components/Events.js
@@ -8,7 +8,7 @@ export default function Events(){
     <section id="events">
       <div className="event-container">
         <h1>EVENTS</h1>
-        <div className="box">
+        <div className="box-events">
           <div className="items" >
             <img src={Image} />
             <h2>SIMULATOR</h2>
