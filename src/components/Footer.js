c178e8951936d3a85a29242bb73008c24241cf7d
Paarth <parthbansal2047@gmail.com>
2023-02-11 12:44:18 +0530

    Completed HTML for Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,28 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons'
+
 
 export default function Footer(){
     return(
-        <h1>Footer</h1>
+        <div className='footer-container'>
+            <div>
+                <h1 className='footer-title'> Embedded Systems <br></br>and{" "}
+                <span className="footer-robotics-text">Robotics</span></h1>
+                <button className='footer-contact-button'><a href='#Contact'>Contact Us</a></button>
+            </div>
+            <div>
+                <a href='#Home' className='footer-link'>Home</a>
+                <a href='#Events' className='footer-link'>Events</a>
+                <a href='#Team' className='footer-link'>Team</a>
+                <a href='#Workshops' className='footer-link'>Workshops</a>
+            </div>
+            <div>
+                <a href="#Linkedin" className='footer-icons'><FontAwesomeIcon icon={faLinkedin} /></a>
+                <a href="#Github" className='footer-icons'><FontAwesomeIcon icon={faGithub} /></a>
+                <a href="#Instagram" className='footer-icons'><FontAwesomeIcon icon={faInstagram} /></a>
+            </div>
+        </div>
     )
 }
